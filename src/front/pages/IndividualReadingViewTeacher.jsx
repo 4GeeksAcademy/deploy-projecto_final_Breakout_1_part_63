407a88f2bdc6ef208548ceac64cf83482963c36e
VicenteAGT <155429830+VicenteAGT@users.noreply.github.com>
2026-02-17 14:59:47 +0000

    [ultimates fixes]

--- src/front/pages/IndividualReadingViewTeacher.jsx
+++ src/front/pages/IndividualReadingViewTeacher.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { useParams, Link } from "react-router-dom";
-import portada from "../assets/img/portada.png";
+
 
 export const IndividualReadingViewTeacher = () => {
 
